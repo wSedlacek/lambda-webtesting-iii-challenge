b8e409781db475a3ed876d599c4bdb684cf1ee9d
William Sedlacek <wsedlacekc@gmail.com>
2019-10-14 17:42:45 -0500

    Remove unused wait
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/controls/Controls.spec.tsx
+++ src/controls/Controls.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, wait } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Controls from './Controls';
 
